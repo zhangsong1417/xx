1f2fe532d21445ff8b9565d15669a412723b2aa0
zhangsong <44634576+shuiling21@users.noreply.github.com>
2019-03-26 17:23:30 +0800

    Create open.py

---
+++ live/open.py
@@ -0,0 +1,11 @@
+import os
+
+
+def video_change(video_path, out_path,fps):
+    ff = FFmpeg(inputs={video_path:None},
+                outputs={out_path: ''}
+    pass
+
+
+if __name__ == '__main__':
+    pass
