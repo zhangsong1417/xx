4a50891a1e6215b892e4bf5aa3b76f4bf0ab1ab6
zhangsong <44634576+shuiling21@users.noreply.github.com>
2019-06-21 13:44:54 +0800

    Create test_login.py

---
+++ Python_script/Autotest_Project/Website/test_case/test_login.py
@@ -0,0 +1,49 @@
+import unittest
+from model import function,myunit
+from page_object.LoginPage import *
+from time import sleep
+
+
+class LoginTest(myunit.StartEnd):
+    # @unittest.skip('skip this case')
+    def test_login1_normal(self):
+        '''username password is normal'''
+        print("test_login1_normal is start run...")
+        po = LoginPage(self.driver)
+        po.Login_action('51zxw', 123456)
+
+       # sleep(5)
+
+        # 断言与截屏
+        self.assertEqual(po.type_loginPass_hint(), "我的空间")
+        function.insert_img(self.driver, '51zxw_login1_normal.jpg')
+        print("test_login1_normal is test end!")
+
+    def test_login2_passwdError(self):
+        '''username is ok,password is error!'''
+        print("test_login2_PasswdError is start run"
+              ""
+              ""
+              ""
+              ""
+              ""
+              ""
+              "")
+        po = LoginPage(self.driver)
+        po.Login_action("51zxw",12342)
+        sleep(2)
+
+        self.assertEqual(po.type_loginFail_hint(), '')
+        function.insert_img(self.driver, "51zxw_login2_fail.jpg")
+        print("test_login2_PasswdError is test end!")
+
+    def test_login3_empty(self):
+        '''username password is empty'''
+        print("test_login3_empty is start run...")
+        po = LoginPage(self.driver)
+        po.Login_action("", "")
+        sleep(2)
+
+        self.assertEqual(po.type_loginFail_hint(), '')
+        function.insert_img(self.driver, "51zxw_login3_fail.jpg")
+        print("test_login3_empty is test end!")
