abca5e780327fd550d062cfee78f6f02fab2a6fe
zhangsong <44634576+shuiling21@users.noreply.github.com>
2019-06-21 13:40:08 +0800

    Create LoginClass.py

---
+++ Python_script/51zxw/Test_Module/LoginClass.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from time import sleep
+
+
+class Login():
+    def user_login(self, driver):
+        # 输入用户名
+        driver.find_element_by_name('username').clear()
+        driver.find_element_by_name('username').send_keys('51zxw')
+        # 输入密码
+        driver.find_element_by_name('password').clear()
+        driver.find_element_by_name('password').send_keys('123456')
+        # 点击登录
+        driver.find_element_by_name('Submit').click()
+
+    def user_logout(self, driver):
+        driver.find_element_by_link_text('退出').click()
+        sleep(5)
+        # 退出
+        driver.switch_to_alert().accept()
+
+
+if __name__ == '__main__':
+    driver = webdriver.Firefox()
+    driver.get("http://localhost:8080/")
+    driver.implicitly_wait(10)
+
+    Login().user_login(driver)
+    sleep(2)
+    Login().user_logout(driver)
+
+    sleep(5)
+    driver.quit()
