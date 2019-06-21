fa9fa4abcc0e58ef7eaaec37dbddb58751bd8302
zhangsong <44634576+shuiling21@users.noreply.github.com>
2019-06-21 13:43:54 +0800

    Create test_add.py

---
+++ Python_script/51zxw/unittest/Test_Project/test_add.py
@@ -0,0 +1,12 @@
+from calculator import *
+from StartEnd import *
+
+
+class Test_add(Setup_tearDown):
+    def test_add(self):
+        j = Math(5, 5)
+        self.assertEqual(j.add(), 10)
+
+    def test_add1(self):
+        j = Math(8, 18)
+        self.assertEqual(j.add(), 16)
